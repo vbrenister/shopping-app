6bdbd9eaa6d01c66d79404a97bdff06df52e46d1
vbrenister <Victor.Brenister@endava.com>
2019-04-09 10:38:17 +0300

    Added main routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  featureSelected = 'recipe';
 
-  onNavigate(feature) {
-    this.featureSelected = feature;
-  }
 }
